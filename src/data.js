5addeaba2dba331c9ff98e93b41c2c025c9ce2e4
Somraj Mukherjee <iamsomraj@gmail.com>
2021-06-02 18:49:23 +0530

    Added new questions

--- src/data.js
+++ src/data.js
@@ -1,20 +1,20 @@
 const questions = [
   {
     id: "q1",
-    label: "How are you?",
+    label: "How you feel today?",
     options: [
-      { id: "a", label: "a" },
-      { id: "b", label: "b" },
-      { id: "c", label: "c" }
+      { id: "a", label: "Brilliant! I have so much energy" },
+      { id: "b", label: "Always can be worse" },
+      { id: "c", label: "Please, end my misery" }
     ]
   },
   {
     id: "q2",
-    label: "How are you?",
+    label: "How you like the Opinary test?",
     options: [
-      { id: "a", label: "a" },
-      { id: "b", label: "b" },
-      { id: "c", label: "c" }
+      { id: "a", label: "It was great and so challenging" },
+      { id: "b", label: "Not bad, but you can improve" },
+      { id: "c", label: "It was a nightmare, never again" }
     ]
   }
 ];
